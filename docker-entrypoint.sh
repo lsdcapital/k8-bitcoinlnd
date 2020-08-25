c9db8df0fa12d8347c3a23a0afd5a2821fae3e43
Stefan Lesicnik <stefan@lsdopen.io>
2020-08-25 22:09:49 +0200

    -Update entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,24 +3,25 @@ set -e
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
   echo "$0: assuming arguments for bitcoind"
-
   set -- bitcoind "$@"
+  echo "command line: $@"
 fi
 
 if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
   mkdir -p "$BITCOIN_DATA"
   chmod 700 "$BITCOIN_DATA"
-  chown -R bitcoin "$BITCOIN_DATA"
+  chown -R 1000 "$BITCOIN_DATA"
 
   echo "$0: setting data directory to $BITCOIN_DATA"
 
   set -- "$@" -datadir="$BITCOIN_DATA"
+  echo "command line: $@"
 fi
 
 if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
   echo
-  exec bitcoin "$@"
+  exec gosu 1000 "$@"
 fi
 
-echo
+echo 
 exec "$@"
