efe953c6f1a137cfa11e04dacd050acc6378a50e
Stefan Lesicnik <stefan@lsd.co.za>
2020-09-07 17:18:54 +0200

    Full path to NPM in entryfile

--- docker/lndhub/entrypoint.sh
+++ docker/lndhub/entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 # Assuming arguments for lndhub
 if [ $(echo "$1" | cut -c1) = "-" ]; then
-  exec "npm start --prefix /lndhub $@"
+  exec "/usr/local/bin/npm start --prefix /lndhub $@"
 else
   # lndhub standard launch with or without params
-  if [ "$1" = "npm start --prefix /lndhub" ]; then
+  if [ "$1" = "/usr/local/bin/npm start --prefix /lndhub" ]; then
     exec "$@"
   else
     # Custom launch
